6d4de1bf88893f1e32b28fd700e91db8b60d4407
Lilei <Lilei@Lilei-PC>
2013-12-19 14:03:11 +0800

    外观模式

---
+++ MyProject2/src/main/java/com/palmaplus2/Facade.java
@@ -0,0 +1,29 @@
+package com.palmaplus2;
+
+import java.io.IOException;
+import com.esri.arcgis.geometry.ISegmentCollection;
+import com.google.protobuf.Message.Builder;
+import com.palmaplus.ocean.PlanarGraphics.Feature;
+
+//外观模式应用
+public class Facade {
+	
+	private FeatureS feature;
+	private Segmentclass segmentclass;
+	
+	public Facade() {
+		feature = new FeatureS();
+		segmentclass = new Segmentclass();
+	}
+	
+	public void setBuilder(ISegmentCollection segmentCollection,
+			Builder bui) throws IOException {
+		segmentclass.setBuilder(segmentCollection, bui);
+	}
+	
+	public void setFeatureBuilder(Object column_value, 
+			Feature.Builder ownedFeatureBuilder) {
+		feature.setFeatureBuilder(column_value, ownedFeatureBuilder);
+		
+	}
+}
